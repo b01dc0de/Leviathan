0385aa12925aca887f94d122aaa90657c3113195
CKA <b01dc0de@protonmail.com>
2024-05-01 14:07:03 -0500

    Add basic WinMain and WinProc behavior to display an empty window Keep in mind this breaks Linux

--- HelloTriangle/src/main.cpp
+++ HelloTriangle/src/main.cpp
@@ -1,11 +1,94 @@
 #include <cstdio>
 
-int main(int argc, const char* argv[])
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+bool bGlobalRunning = false;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
 {
-	printf("BEGIN HelloTriangle:\n");
-	for (int i = 1; i < argc; i++)
+	LPCWSTR AppName = L"HelloTriangle";
+
+	WNDCLASS WndClass =
 	{
-		printf("\tArg %d: %s\n", i, argv[i]);
+		CS_GLOBALCLASS | CS_HREDRAW | CS_VREDRAW,
+		WindowProc,
+		0,
+		0,
+		hInstance,
+		0,
+		0,
+		0,
+		nullptr,
+		AppName
+	};
+
+	ATOM RegClassResult = RegisterClass(&WndClass);
+
+	HINSTANCE hInst = hInstance;
+	HWND hWindow = CreateWindowEx(
+		WS_EX_OVERLAPPEDWINDOW,
+		AppName,
+		AppName,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		nullptr,
+		nullptr,
+		hInst,
+		nullptr // CKA_NOTE: Might want to use this in the future
+	);
+
+	if (hWindow)
+	{
+		ShowWindow(hWindow, nCmdShow);
+		UpdateWindow(hWindow);
+
+		bGlobalRunning = true;
+		while (bGlobalRunning)
+		{
+			MSG msg;
+			BOOL MsgResult;
+			while ((MsgResult = PeekMessage(&msg, hWindow, 0, 0, PM_REMOVE)) > 0)
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			if (MsgResult == -1)
+			{
+				// CKA_TODO: Error handling?
+				//bGlobalRunning = false;
+				break;
+			}
+		}
 	}
-	printf("END HelloTriangle\n");
+	else
+	{
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	LRESULT Result;
+	switch (uMsg)
+	{
+		case WM_LBUTTONDOWN:
+		case WM_RBUTTONDOWN:
+		case WM_KEYDOWN:
+		{
+			bGlobalRunning = false;
+		}
+		default:
+		{
+			Result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		} break;
+	}
+	
+	return Result;
 }
