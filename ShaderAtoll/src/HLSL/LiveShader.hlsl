0bb82e7fb344d622000e2c5cebadbea92dead71a
CKA <163610791+b01dc0de@users.noreply.github.com>
2024-09-04 18:13:24 -0500

    ShaderAtoll: Add available global variables as context in comments for LiveShader.hlsl

--- ShaderAtoll/src/HLSL/LiveShader.hlsl
+++ ShaderAtoll/src/HLSL/LiveShader.hlsl
@@ -2,6 +2,14 @@
 // LiveShader.hlsl //
 /////////////////////
 
+// Globals available in Main_Live:
+//    float AppTime;
+//    float DeltaTime;
+//    int FrameWidth;
+//    int FrameHeight;
+//    int MouseX;
+//    int MouseY;
+
 float4 Main_Live(float4 ScreenPos)
 {
     return 0.0;
