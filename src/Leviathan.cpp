3dec7776111f2bfb3d9c5b45c0ac3a3ab1f8a8a1
CKA <163610791+b01dc0de@users.noreply.github.com>
2025-01-24 15:22:39 -0600

    Create and show empty window

--- src/Leviathan.cpp
+++ src/Leviathan.cpp
@@ -2,6 +2,101 @@
 
 namespace Leviathan
 {
+#define APPNAMEW() (L"Leviathan")
+#define APPNAMEA() ("Leviathan")
+#if UNICODE
+    #define APPNAME() APPNAMEW()
+#else // UNICODE
+    #define APPNAME() APPNAMEA()
+#endif // UNICODE
+    int AppWidth = 1280;
+    int AppHeight = 720;
+    bool bAppRunning = true;
+    HWND AppWindow = nullptr;
+
+    LRESULT WindowProc(
+        HWND hWnd,
+        UINT uMsg,
+        WPARAM wParam,
+        LPARAM lParam 
+    )
+    {
+        LRESULT lResult = 0;
+        switch (uMsg)
+        {
+            case WM_KEYUP:
+            {
+                if (wParam == VK_ESCAPE)
+                {
+                    bAppRunning = false;
+                }
+            } break;
+            case WM_CLOSE:
+            {
+                bAppRunning = false;
+            }
+            default:
+            {
+                lResult = DefWindowProc(hWnd, uMsg, wParam, lParam);
+            } break;
+        }
+        return lResult;
+    }
+
+    HWND InitWindow(HINSTANCE hInst, int Width, int Height)
+    {
+        WNDCLASSEX WndClassEx = {};
+        WndClassEx.cbSize = sizeof(WNDCLASSEXA);
+        WndClassEx.style = CS_HREDRAW | CS_VREDRAW;
+        WndClassEx.lpfnWndProc = WindowProc;
+        WndClassEx.cbClsExtra = 0;
+        WndClassEx.cbWndExtra = 0;
+        WndClassEx.hIcon = nullptr;
+        WndClassEx.hCursor = nullptr;
+        WndClassEx.hbrBackground = nullptr;
+        WndClassEx.lpszMenuName = nullptr;
+        WndClassEx.lpszClassName = APPNAME();
+        WndClassEx.hIconSm = nullptr;
+        RegisterClassEx(&WndClassEx);
+
+        DWORD WndExStyle = WS_EX_OVERLAPPEDWINDOW;
+        DWORD WndStyle = WS_CAPTION | WS_OVERLAPPEDWINDOW;
+
+        RECT WndRect = { 0, 0, (LONG)Width, (LONG)Height };
+        AdjustWindowRectEx(&WndRect, WndStyle, FALSE, WndExStyle);
+        // CKA_NOTE: AdjustWindowRectExForDpi
+        LONG AdjustedWidth = WndRect.right - WndRect.left;
+        LONG AdjustedHeight = WndRect.bottom - WndRect.top;
+
+        return CreateWindowEx(
+            WndExStyle,
+            APPNAME(),
+            APPNAME(),
+            WndStyle,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            AdjustedWidth,
+            AdjustedHeight,
+            nullptr,
+            nullptr,
+            hInst,
+            nullptr /* lpParam */
+        );
+    }
+
+    int WindowMsgLoop(HWND InWindow)
+    {
+        int MsgCount = 0;
+        MSG Msg = {};
+        while (PeekMessage(&Msg, InWindow, 0, 0, PM_REMOVE) > 0)
+        {
+            TranslateMessage(&Msg);
+            DispatchMessage(&Msg);
+            MsgCount++;
+        }
+        return MsgCount;
+    }
+
     int WinMain
     (
         HINSTANCE hInstance,
@@ -11,6 +106,20 @@ namespace Leviathan
     )
     {
         OutputDebugStringA("LEVIATHAN -- INIT\n");
+
+        HWND hWnd = InitWindow(hInstance, AppWidth, AppHeight);
+        if (hWnd)
+        {
+            AppWindow = hWnd;
+            ShowWindow(AppWindow, nShowCmd);
+
+            bAppRunning = true;
+            while (bAppRunning)
+            {
+                WindowMsgLoop(AppWindow);
+            }
+        }
+
         return 0;
     }
 }
@@ -25,3 +134,4 @@ int WinMain
 {
     return Leviathan::WinMain(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
 }
+
--- src/Leviathan.h
+++ src/Leviathan.h
@@ -3,5 +3,9 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <dxgi.h>
 
 #endif // LEVIATHAN_H
+
