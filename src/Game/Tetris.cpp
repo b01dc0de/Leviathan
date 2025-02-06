82be77e67e124e5ee3cb5ec8e0da7420a2c24a63
CKA <163610791+b01dc0de@users.noreply.github.com>
2025-02-06 14:04:47 -0600

    Add "beginnings" of a Tetris game

---
+++ src/Game/Tetris.cpp
@@ -0,0 +1,198 @@
+#include "Tetris.h"
+#include "../Leviathan.h"
+
+namespace Game
+{
+    namespace Tetris
+    {
+        enum BlockType
+        {
+            BLOCK_NONE,
+            BLOCK_I,
+            BLOCK_O,
+            BLOCK_T,
+            BLOCK_S,
+            BLOCK_Z,
+            BLOCK_J,
+            BLOCK_L,
+            BLOCKTYPE_COUNT
+        };
+
+        constexpr int OrientationI_Length = 4;
+        constexpr int OrientationO_Length = 2;
+        constexpr int Orientation_Length = 3;
+
+        constexpr int OrientationI_Size = OrientationI_Length * OrientationI_Length;
+        constexpr int OrientationO_Size = OrientationO_Length * OrientationO_Length;
+        constexpr int Orientation_Size = Orientation_Length * Orientation_Length;
+
+        BlockType Orientations_I[][OrientationI_Size] =
+        {
+            {
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE, BLOCK_NONE,
+                   BLOCK_I,    BLOCK_I,    BLOCK_I,    BLOCK_I,
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE, BLOCK_NONE,
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE, BLOCK_NONE,
+            },
+            {
+                BLOCK_NONE, BLOCK_NONE,    BLOCK_I, BLOCK_NONE,
+                BLOCK_NONE, BLOCK_NONE,    BLOCK_I, BLOCK_NONE,
+                BLOCK_NONE, BLOCK_NONE,    BLOCK_I, BLOCK_NONE,
+                BLOCK_NONE, BLOCK_NONE,    BLOCK_I, BLOCK_NONE,
+            },
+            {
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE, BLOCK_NONE,
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE, BLOCK_NONE,
+                   BLOCK_I,    BLOCK_I,    BLOCK_I,    BLOCK_I,
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE, BLOCK_NONE,
+            },
+            {
+                BLOCK_NONE,    BLOCK_I, BLOCK_NONE, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_I, BLOCK_NONE, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_I, BLOCK_NONE, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_I, BLOCK_NONE, BLOCK_NONE,
+            },
+        };
+        BlockType Orientations_O[][OrientationO_Size] =
+        {
+            {
+                BLOCK_O, BLOCK_O,
+                BLOCK_O, BLOCK_O,
+            }
+        };
+        BlockType Orientations_T[][Orientation_Size] =
+        {
+            {
+                BLOCK_NONE,    BLOCK_T, BLOCK_NONE,
+                   BLOCK_T,    BLOCK_T,    BLOCK_T,
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE
+            },
+            {
+                BLOCK_NONE,    BLOCK_T, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_T,    BLOCK_T,
+                BLOCK_NONE,    BLOCK_T, BLOCK_NONE
+            },
+            {
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE,
+                   BLOCK_T,    BLOCK_T,    BLOCK_T,
+                BLOCK_NONE,    BLOCK_T, BLOCK_NONE
+            },
+            {
+                BLOCK_NONE,    BLOCK_T, BLOCK_NONE,
+                   BLOCK_T,    BLOCK_T, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_T, BLOCK_NONE
+            }
+        };
+        BlockType Orientations_S[][Orientation_Size] =
+        {
+            {
+                BLOCK_NONE,    BLOCK_S,    BLOCK_S,
+                   BLOCK_S,    BLOCK_S, BLOCK_NONE,
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE
+            },
+            {
+                BLOCK_NONE,    BLOCK_S, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_S,    BLOCK_S,
+                BLOCK_NONE, BLOCK_NONE,    BLOCK_S
+            },
+            {
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_S,    BLOCK_S,
+                   BLOCK_S,    BLOCK_S, BLOCK_NONE
+            },
+            {
+                   BLOCK_S, BLOCK_NONE, BLOCK_NONE,
+                   BLOCK_S,    BLOCK_S, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_S, BLOCK_NONE
+            }
+        };
+        BlockType Orientations_Z[][Orientation_Size] =
+        {
+            {
+                   BLOCK_Z,    BLOCK_Z, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_Z,    BLOCK_Z,
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE
+            },
+            {
+                BLOCK_NONE, BLOCK_NONE,    BLOCK_Z,
+                BLOCK_NONE,    BLOCK_Z,    BLOCK_Z,
+                BLOCK_NONE,    BLOCK_Z, BLOCK_NONE
+            },
+            {
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE,
+                   BLOCK_Z,    BLOCK_Z, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_Z,    BLOCK_Z
+            },
+            {
+                BLOCK_NONE,    BLOCK_Z, BLOCK_NONE,
+                   BLOCK_Z,    BLOCK_Z, BLOCK_NONE,
+                   BLOCK_Z, BLOCK_NONE, BLOCK_NONE
+            }
+        };
+        BlockType Orientations_J[][Orientation_Size] =
+        {
+            {
+                   BLOCK_J, BLOCK_NONE, BLOCK_NONE,
+                   BLOCK_J,    BLOCK_J,    BLOCK_J,
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE
+            },
+            {
+                BLOCK_NONE,    BLOCK_J,    BLOCK_J,
+                BLOCK_NONE,    BLOCK_J, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_J, BLOCK_NONE
+            },
+            {
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE,
+                   BLOCK_J,    BLOCK_J,    BLOCK_J,
+                BLOCK_NONE, BLOCK_NONE,    BLOCK_J
+            },
+            {
+                BLOCK_NONE,    BLOCK_J, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_J, BLOCK_NONE,
+                   BLOCK_J,    BLOCK_J, BLOCK_NONE
+            }
+        };
+        BlockType Orientations_L[][Orientation_Size] =
+        {
+            {
+                BLOCK_NONE, BLOCK_NONE,    BLOCK_L,
+                   BLOCK_L,    BLOCK_L,    BLOCK_L,
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE
+            },
+            {
+                BLOCK_NONE,    BLOCK_L, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_L, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_L,    BLOCK_L
+            },
+            {
+                BLOCK_NONE, BLOCK_NONE, BLOCK_NONE,
+                   BLOCK_L,    BLOCK_L,    BLOCK_L,
+                   BLOCK_L, BLOCK_NONE, BLOCK_NONE
+            },
+            {
+                   BLOCK_L,    BLOCK_L, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_L, BLOCK_NONE,
+                BLOCK_NONE,    BLOCK_L, BLOCK_NONE
+            }
+        };
+
+        constexpr int GridWidth = 10;
+        constexpr int GridHeight = 20;
+        constexpr int GridSize = GridWidth * GridHeight;
+        BlockType PlayField[GridSize];
+
+        void UpdateAndDraw()
+        {
+        }
+
+        void Init()
+        {
+            for (int CellIdx = 0; CellIdx < GridSize; CellIdx++) { PlayField[CellIdx] = BLOCK_NONE; }
+        }
+
+        void Term()
+        {
+
+        }
+    }
+}
---
+++ src/Game/Tetris.h
@@ -0,0 +1,14 @@
+#ifndef GAME_TETRIS_H
+#define GAME_TETRIS_H
+
+namespace Game
+{
+    namespace Tetris
+    {
+        void UpdateAndDraw();
+        void Init();
+        void Term();
+    }
+}
+
+#endif // GAME_TETRIS_H
