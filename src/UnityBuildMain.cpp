e8d87a47ac56a06f8b177d3380f60ec1720bd749
CKA <163610791+b01dc0de@users.noreply.github.com>
2025-04-15 16:53:14 -0500

    Inline src/ files in vcxproj filters, fix unity build

--- src/UnityBuildMain.cpp
+++ src/UnityBuildMain.cpp
@@ -5,6 +5,7 @@
 #endif // ifndef UNITY_BUILD
 
 #if BUILD_UNITY
+    #include "BatchDraw.cpp"
     #include "Camera.cpp"
     #include "Clock.cpp"
     #include "DrawState.cpp"
@@ -16,15 +17,18 @@
     #include "Input.cpp"
     #include "InputVisualizer.cpp"
     #include "Math.cpp"
-    #include "MeshState.h"
+    #include "MeshState.cpp"
     #include "PerformanceGraph.cpp"
     #include "UserInterface.cpp"
     #include "Utils.cpp"
     #include "Game/GameManager.cpp"
+    // Main:
     #include "Leviathan.cpp"
 
     #include "Game/BulletLimbo.cpp"
-    #include "Game/Tetris.cpp"
+    #include "Game/Labyrinth.cpp"
     #include "Game/Snake.cpp"
+    #include "Game/SpeedCube.cpp"
+    #include "Game/Tetris.cpp"
 #endif // BUILD_UNITY()
 
