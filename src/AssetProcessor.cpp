f9f2debe3b4194293dd4fe71c37cbf30c538fb3d
CKA <163610791+b01dc0de@users.noreply.github.com>
2025-05-23 15:10:07 -0500

    AssetProcessor: Construct per-face data while parsing

--- src/AssetProcessor.cpp
+++ src/AssetProcessor.cpp
@@ -6,22 +6,27 @@ namespace Leviathan
 namespace MeshOBJ
 {
 
-struct FaceIndices
+struct PerFaceData
 {
-    static constexpr int MaxVerts = 4;
-    int NumVerts;
-    int vIdx[MaxVerts];
-    //int tIdx[MaxVerts];
-    //int nIdx[MaxVerts];
+    int vIdx0;
+    int tIdx0;
+    int nIdx0;
+
+    int vIdx1;
+    int tIdx1;
+    int nIdx1;
+
+    int vIdx2;
+    int tIdx2;
+    int nIdx2;
 };
 
 struct ParsedOBJ
 {
     Array<v4f> Vertices;
-    //Array<v2f> TexUVs;
-    //Array<v3f> Normals;
-    Array<FaceIndices> Faces;
-    int NumPrimitives = 0;
+    Array<v2f> TexUVs;
+    Array<v3f> Normals;
+    Array<PerFaceData> Faces;
 };
 
 MeshStateT CreateMeshStateFromOBJ(ParsedOBJ& OBJ)
@@ -31,7 +36,7 @@ MeshStateT CreateMeshStateFromOBJ(ParsedOBJ& OBJ)
 
     // TODO(CKA): Implement support for texture (+ normal!) vertices
     int NumVerts = OBJ.Vertices.Num;
-    int NumInds = OBJ.NumPrimitives * 3;
+    int NumInds = OBJ.Faces.Num * 3;
     VxMin* Verts = new VxMin[NumVerts];
     unsigned int* Inds = new unsigned int[NumInds];
 
@@ -42,16 +47,10 @@ MeshStateT CreateMeshStateFromOBJ(ParsedOBJ& OBJ)
     int IndIdx = 0;
     for (int fIdx = 0; fIdx < OBJ.Faces.Num; fIdx++)
     {
-        ASSERT(OBJ.Faces[fIdx].NumVerts >= 3);
-        int NumTris = OBJ.Faces[fIdx].NumVerts - 2;
-        for (int TriIdx = 0; TriIdx < NumTris; TriIdx++)
-        {
-            ASSERT(IndIdx < NumInds);
-            Inds[IndIdx + 0] = OBJ.Faces[fIdx].vIdx[0];
-            Inds[IndIdx + 1] = OBJ.Faces[fIdx].vIdx[TriIdx + 1];
-            Inds[IndIdx + 2] = OBJ.Faces[fIdx].vIdx[TriIdx + 2];
-            IndIdx += 3;
-        }
+        Inds[IndIdx + 0] = OBJ.Faces[fIdx].vIdx0;
+        Inds[IndIdx + 1] = OBJ.Faces[fIdx].vIdx1;
+        Inds[IndIdx + 2] = OBJ.Faces[fIdx].vIdx2;
+        IndIdx += 3;
     }
 
     MeshStateT Result = CreateMeshState
@@ -74,15 +73,34 @@ MeshStateT ParseOBJFile(byte* Contents, size_t Size)
 {
     ParsedOBJ OBJ;
 
+    struct FaceInds
+    {
+        int vIdx = 0;
+        int tIdx = 0;
+        int nIdx = 0;
+    };
+    Array<FaceInds> CurrFace;
+
     bool bError = false;
     int ReadIdx = 0;
     while (!bError && ReadIdx < Size)
     {
         switch (Contents[ReadIdx])
         {
+            case 'm':
+            case 'o':
+            case 'u': break;
             case 'v':
             {
-                if (Contents[ReadIdx + 1] != ' ') { break; } // Not vertex position data
+                if (Contents[ReadIdx + 1] == 't') { break; } // vertex texture
+                if (Contents[ReadIdx + 1] == 'n') { break; } // vertex normal
+                else if (Contents[ReadIdx + 1] == ' ') { } // vertex position
+                else
+                {
+                    // not vertex position data
+                    ASSERT(false);
+                    bError = true;
+                }
 
                 v4f NewVx{ 0.0f, 0.0f, 0.0f, 1.0f };
                 {
@@ -116,18 +134,22 @@ MeshStateT ParseOBJFile(byte* Contents, size_t Size)
                 char* DigitBegin = (char*)Contents + ReadIdx + 1;
                 char* AfterDigit = nullptr;
 
-                FaceIndices NewFace{};
-                while (!bError && (NewFace.NumVerts < FaceIndices::MaxVerts) && (ReadIdx < Size && *DigitBegin && *DigitBegin != '\n' && *DigitBegin != '\r'))
+                CurrFace.Empty();
+                while (!bError && (ReadIdx < Size && *DigitBegin && *DigitBegin != '\n' && *DigitBegin != '\r'))
                 {
                     int NewIndex = strtol(DigitBegin, &AfterDigit, 10);
 
                     if (NewIndex > 0 && AfterDigit)
                     {
-                        NewFace.vIdx[NewFace.NumVerts++] = NewIndex - 1;
+                        FaceInds NewFaceInds{};
+                        NewFaceInds.vIdx = NewIndex - 1;
                         if (*AfterDigit == '/') { 
                             do { AfterDigit++; }
                             while (*AfterDigit && *AfterDigit != ' ' && *AfterDigit != '\n' && *AfterDigit != '\r');
                         }
+
+                        CurrFace.Add(NewFaceInds);
+
                         DigitBegin = AfterDigit;
                         AfterDigit = nullptr;
                     }
@@ -138,19 +160,35 @@ MeshStateT ParseOBJFile(byte* Contents, size_t Size)
                     }
                 }
 
-                if (NewFace.NumVerts > 2) { OBJ.NumPrimitives += NewFace.NumVerts - 2; }
-                else { bError = true; }
-
-                if (!bError)
+                if (CurrFace.Num > 2)
                 {
-                    OBJ.Faces.Add(NewFace);
+                    PerFaceData NewFace{};
+                    NewFace.vIdx0 = CurrFace[0].vIdx;
+                    NewFace.tIdx0 = CurrFace[0].tIdx;
+                    NewFace.nIdx0 = CurrFace[0].vIdx;
+
+                    int NumFaces = CurrFace.Num - 2;
+                    for (int FaceIdx = 0; FaceIdx < NumFaces; FaceIdx++)
+                    {
+                        NewFace.vIdx1 = CurrFace[FaceIdx + 1].vIdx;
+                        NewFace.tIdx1 = CurrFace[FaceIdx + 1].tIdx;
+                        NewFace.nIdx1 = CurrFace[FaceIdx + 1].nIdx;
+
+                        NewFace.vIdx2 = CurrFace[FaceIdx + 2].vIdx;
+                        NewFace.tIdx2 = CurrFace[FaceIdx + 2].tIdx;
+                        NewFace.nIdx2 = CurrFace[FaceIdx + 2].nIdx;
+                        OBJ.Faces.Add(NewFace);
+                    }
                 }
+                else { bError = true; }
             } break;
         }
 
         // Read to next line
         while (ReadIdx < Size && Contents[ReadIdx] && Contents[ReadIdx] != '\n') { ReadIdx++; } ReadIdx++;
     }
+
+    ASSERT(!bError);
 
     MeshStateT Result{};
     if (!bError)
