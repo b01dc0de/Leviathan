2ec17a9834657f6d189cd7b41a85366435ba856e
CKA <163610791+b01dc0de@users.noreply.github.com>
2025-05-23 18:32:45 -0500

    UserInterface: Draw frame time stats independent of perf graph

--- src/UserInterface.cpp
+++ src/UserInterface.cpp
@@ -17,12 +17,12 @@ namespace UserInterfaceState
 using namespace UserInterfaceState;
 
 bool bVisualizeInput = false;
-bool bDrawTimeStats = false;
+bool bDrawPerfGraph = false;
 
 void UserInterface::Draw(BatchDrawCmds& Draw2D, LvFont& InFont)
 {
     if (KeyboardState::GetKey(LV_KEY_F1)) { bVisualizeInput = !bVisualizeInput; }
-    if (KeyboardState::GetKey(LV_KEY_F2)) { bDrawTimeStats = !bDrawTimeStats; }
+    if (KeyboardState::GetKey(LV_KEY_F2)) { bDrawPerfGraph = !bDrawPerfGraph; }
 
     if (bVisualizeInput)
     {
@@ -31,15 +31,25 @@ void UserInterface::Draw(BatchDrawCmds& Draw2D, LvFont& InFont)
         InputVisualizer::DrawGamepads(Draw2D);
     }
 
+    if (bDrawPerfGraph)
+    {
+        PerformanceGraph::Draw(Draw2D);
+    }
+
+    static constexpr bool bDrawTimeStats = true;
     if (bDrawTimeStats)
     {
-        static constexpr int MsgBufferSize = 64;
+        constexpr v2f Origin{ 1100.0f, 0.0f };
+        constexpr int MsgBufferSize = 64;
+
         char MsgBuffer[MsgBufferSize];
         double CurrTime = Clock::Time();
         int MsgLength = sprintf_s(MsgBuffer, "CurrTime:%.02f", CurrTime);
-        constexpr v2f Origin{ 1100.0f, 720.0f * 0.46f };
         InFont.Draw(Draw2D, Origin, 1.5f, MsgBuffer, MsgLength);
-        PerformanceGraph::Draw(Draw2D);
+
+        double DeltaTimeMs = Clock::DeltaTime() * 1000.0;
+        MsgLength = sprintf_s(MsgBuffer, "Frame:%.02f ms", DeltaTimeMs);
+        InFont.Draw(Draw2D, Origin + v2f{ 0.0f, 16.0f }, 1.5f, MsgBuffer, MsgLength);
     }
 }
 
