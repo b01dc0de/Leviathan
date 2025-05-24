abf446dfa62af7fb7c37a5a56be998644ced5331
CKA <163610791+b01dc0de@users.noreply.github.com>
2025-05-24 00:07:47 -0500

    Adjust time stats' draw location based off length of displayed text

--- src/UserInterface.cpp
+++ src/UserInterface.cpp
@@ -39,17 +39,21 @@ void UserInterface::Draw(BatchDrawCmds& Draw2D, LvFont& InFont)
     static constexpr bool bDrawTimeStats = true;
     if (bDrawTimeStats)
     {
-        constexpr v2f Origin{ 1100.0f, 0.0f };
+        constexpr v2f Origin{ 1280.0f , 0.0f };
+        constexpr float Scale = 2.0f;
+        // NOTE(CKA): These glyph values are _rough_ estimates:
+        constexpr float GlyphHeight = Scale * 10.0f;
+        constexpr float GlyphWidth = Scale * 7.5f;
         constexpr int MsgBufferSize = 64;
 
         char MsgBuffer[MsgBufferSize];
         double CurrTime = Clock::Time();
         int MsgLength = sprintf_s(MsgBuffer, "CurrTime:%.02f", CurrTime);
-        InFont.Draw(Draw2D, Origin, 1.5f, MsgBuffer, MsgLength);
+        InFont.Draw(Draw2D, Origin + v2f{ -GlyphWidth * MsgLength, 0.0f }, Scale, MsgBuffer, MsgLength);
 
         double DeltaTimeMs = Clock::DeltaTime() * 1000.0;
         MsgLength = sprintf_s(MsgBuffer, "Frame:%.02f ms", DeltaTimeMs);
-        InFont.Draw(Draw2D, Origin + v2f{ 0.0f, 16.0f }, 1.5f, MsgBuffer, MsgLength);
+        InFont.Draw(Draw2D, Origin + v2f{ -GlyphWidth * MsgLength, GlyphHeight }, Scale, MsgBuffer, MsgLength);
     }
 }
 
