b885100268414270521f488ac7b750f9514f18b4
CKA <163610791+b01dc0de@users.noreply.github.com>
2025-05-27 14:24:53 -0500

    PNG: Fix issues with reconstructing filtered image data; rename some PNG related types

--- src/ImagePNG.cpp
+++ src/ImagePNG.cpp
@@ -3,7 +3,10 @@
 namespace Leviathan
 {
 
-enum struct PNGChunkType
+namespace PNG
+{
+
+enum struct ChunkType
 {
     Other,
     IHDR,
@@ -12,7 +15,7 @@ enum struct PNGChunkType
     IEND,
 };
 
-struct PNGChunk
+struct Chunk
 {
     u32 Length = 0;
     u32 Type = 0;
@@ -24,7 +27,7 @@ struct PNGChunk
     u32 CRC = 0;
 };
 
-enum struct PNGColorType
+enum struct ColorFormat
 {
     Invalid,
     TruecolorAlpha,
@@ -34,7 +37,7 @@ enum struct PNGColorType
     PaletteIndexed
 };
 
-struct PNGChunkDataIHDR
+struct HeaderData
 {
     u32 Width;
     u32 Height;
@@ -45,20 +48,20 @@ struct PNGChunkDataIHDR
     byte InterlaceMethod;
 };
 
-struct PaletteEntry
+struct PaletteData
 {
-    u8 R;
-    u8 G;
-    u8 B;
-};
+    struct Entry
+    {
+        u8 R;
+        u8 G;
+        u8 B;
+    };
 
-struct PNGChunkDataPLTE
-{
-    PaletteEntry* Entries = nullptr;
+    Entry* Entries = nullptr;
     u8 NumEntries = 0;
 };
 
-struct PNGParseContext
+struct ParseContext
 {
     byte* Contents;
     size_t Size;
@@ -67,13 +70,14 @@ struct PNGParseContext
     bool bReadHeader;
     bool bReadEnd;
     bool bReadingImageData;
-    PNGChunkDataIHDR HeaderChunk;
-    PNGChunkDataPLTE PaletteChunk;
 
+    HeaderData Header;
+    PaletteData Palette;
     Array<byte> ImageData;
+
     static constexpr byte FileSignature[] = { 137, 80, 78, 71, 13, 10, 26, 10 };
 
-    PNGParseContext(FileContentsT& InFileContents, ImageT& OutImage)
+    ParseContext(FileContentsT& InFileContents, ImageT& OutImage)
     {
         Contents = InFileContents.Contents;
         Size = InFileContents.Size;
@@ -82,8 +86,8 @@ struct PNGParseContext
         bReadHeader = false;
         bReadEnd = false;
         bReadingImageData = false;
-        HeaderChunk = {};
-        PaletteChunk = {};
+        Header = {};
+        Palette = {};
 
         Parse();
         ASSERT(!bError);
@@ -109,14 +113,11 @@ struct PNGParseContext
     }
     u32 SwapEndiannessU32(u32 Value)
     {
-        u8 Value0 = (Value & 0x000000FF) >> 0;
-        u8 Value1 = (Value & 0x0000FF00) >> 8;
-        u8 Value2 = (Value & 0x00FF0000) >> 16;
-        u8 Value3 = (Value & 0xFF000000) >> 24;
-        u32 Result = (Value0 << 24) | (Value1 << 16) | (Value2 << 8) | (Value3 << 0);
+        u32 Result = ((Value & 0x000000FF) << 24) | ((Value & 0x0000FF00) << 8) |
+            ((Value & 0x00FF0000) >> 8) | ((Value & 0xFF000000) >> 24);
         return Result;
     }
-    void ReadChunk(PNGChunk& NewChunk)
+    void ReadChunk(Chunk& NewChunk)
     {
         ASSERT(!bError);
         if (!bError)
@@ -138,72 +139,72 @@ struct PNGParseContext
             }
             NewChunk.CRC = SwapEndiannessU32(*(u32*)(Contents + ReadIdx));
             ReadIdx += sizeof(u32);
-
             // TODO(CKA): Verify CRC
+
         }
     }
-    PNGChunkType GetChunkType(PNGChunk& InChunk)
+    ChunkType GetChunkType(Chunk& InChunk)
     {
-        const PNGChunkType ChunkTypeValues[] = { PNGChunkType::IHDR, PNGChunkType::PLTE, PNGChunkType::IEND, PNGChunkType::IDAT, };
+        const ChunkType ChunkTypeValues[] = { ChunkType::IHDR, ChunkType::PLTE, ChunkType::IEND, ChunkType::IDAT, };
         const char* ChunkTypeLiterals[] = { "IHDR", "PLTE", "IEND", "IDAT", };
         ASSERT(ARRAY_SIZE(ChunkTypeValues) == ARRAY_SIZE(ChunkTypeLiterals));
 
-        PNGChunkType Result = PNGChunkType::Other;
+        ChunkType Result = ChunkType::Other;
         for (int ChunkIdx = 0; ChunkIdx < ARRAY_SIZE(ChunkTypeLiterals); ChunkIdx++)
         {
             if (InChunk.Type == *(u32*)ChunkTypeLiterals[ChunkIdx]) { Result = ChunkTypeValues[ChunkIdx]; break; }
         }
         return Result;
     }
-    PNGColorType GetColorType(byte ColorType)
+    ColorFormat GetColorType(byte Type)
     {
-        PNGColorType Result = PNGColorType::Invalid;
-        switch (ColorType)
+        ColorFormat Result = ColorFormat::Invalid;
+        switch (Type)
         {
-            case 0: { Result = PNGColorType::Grayscale; } break;
-            case 2: { Result = PNGColorType::Truecolor; } break;
-            case 3: { Result = PNGColorType::PaletteIndexed; } break;
-            case 4: { Result = PNGColorType::GrayscaleAlpha; } break;
-            case 6: { Result = PNGColorType::TruecolorAlpha; } break;
+            case 0: { Result = ColorFormat::Grayscale; } break;
+            case 2: { Result = ColorFormat::Truecolor; } break;
+            case 3: { Result = ColorFormat::PaletteIndexed; } break;
+            case 4: { Result = ColorFormat::GrayscaleAlpha; } break;
+            case 6: { Result = ColorFormat::TruecolorAlpha; } break;
         }
         return Result;
     }
-    void HandleChunk(PNGChunk& InChunk)
+    void HandleChunk(Chunk& InChunk)
     {
-        PNGChunkType ChunkType = GetChunkType(InChunk);
+        ChunkType ChunkType = GetChunkType(InChunk);
         switch (ChunkType)
         {
-            case PNGChunkType::Other:
+            case ChunkType::Other:
             {
                 if (!bReadHeader) { bError = true; }
                 // Check that we're not skipping a critical chunk
                 ASSERT(InChunk.bAncillary);
                 delete[] InChunk.Data;
             } break;
-            case PNGChunkType::IHDR:
+            case ChunkType::IHDR:
             {
                 ASSERT(!bReadHeader);
                 if (!bReadHeader)
                 {
-                    HeaderChunk = *(PNGChunkDataIHDR*)InChunk.Data;
-                    HeaderChunk.Width = SwapEndiannessU32(HeaderChunk.Width);
-                    HeaderChunk.Height = SwapEndiannessU32(HeaderChunk.Height);
+                    Header = *(HeaderData*)InChunk.Data;
+                    Header.Width = SwapEndiannessU32(Header.Width);
+                    Header.Height = SwapEndiannessU32(Header.Height);
                     bReadHeader = true;
                 }
                 else { bError = true; }
                 delete[] InChunk.Data;
             } break; 
-            case PNGChunkType::PLTE:
+            case ChunkType::PLTE:
             {
-                ASSERT(PaletteChunk.Entries == nullptr);
+                ASSERT(Palette.Entries == nullptr);
                 // Palette chunk must come before any IDAT chunks
                 ASSERT(ImageData.Num == 0);
                 bool bValidPaletteSize = InChunk.Length % 3 == 0;
                 ASSERT(bValidPaletteSize);
-                if (bReadHeader && ImageData.Num == 0 && PaletteChunk.Entries == nullptr && bValidPaletteSize)
+                if (bReadHeader && ImageData.Num == 0 && Palette.Entries == nullptr && bValidPaletteSize)
                 {
-                    PaletteChunk.Entries = (PaletteEntry*)InChunk.Data;
-                    PaletteChunk.NumEntries = InChunk.Length / 3;
+                    Palette.Entries = (PaletteData::Entry*)InChunk.Data;
+                    Palette.NumEntries = InChunk.Length / 3;
                 }
                 else
                 {
@@ -211,13 +212,13 @@ struct PNGParseContext
                     delete[] InChunk.Data;
                 }
             } break;
-            case PNGChunkType::IEND:
+            case ChunkType::IEND:
             {
                 if (bReadHeader) { bReadEnd = true; }
                 else { bError = true; }
                 ASSERT(InChunk.Length == 0 && InChunk.Data == nullptr);
             } break;
-            case PNGChunkType::IDAT:
+            case ChunkType::IDAT:
             {
                 ASSERT(ImageData.Num == 0 || bReadingImageData);
                 if (ImageData.Num == 0) { bReadingImageData = true; }
@@ -236,7 +237,7 @@ struct PNGParseContext
                 ASSERT(false);
             } break;
         }
-        if (bReadingImageData && ChunkType != PNGChunkType::IDAT) { bReadingImageData = false; }
+        if (bReadingImageData && ChunkType != ChunkType::IDAT) { bReadingImageData = false; }
     }
     void Parse()
     {
@@ -248,7 +249,7 @@ struct PNGParseContext
             ReadIdx = ARRAY_SIZE(FileSignature);
             while (!bError && !bReadEnd && ReadIdx < Size)
             {
-                PNGChunk NewChunk{};
+                Chunk NewChunk{};
                 ReadChunk(NewChunk);
                 HandleChunk(NewChunk);
             }
@@ -289,7 +290,6 @@ struct PNGParseContext
             ASSERT(BlockType != 3); // Reserved / invalid BTYPE
             if (BlockType == 0) // No compression
             {
-                // TODO(CKA): Actually test this code path with uncompressed PNG image data
                 u16 LEN = *(u16*)(ImageData.Data + ByteReadIdx + 1);
                 u16 NLEN = *(u16*)(ImageData.Data + ByteReadIdx + 3);
                 ASSERT((ByteReadIdx + LEN + 5) <= ImageData.Num);
@@ -328,6 +328,65 @@ struct PNGParseContext
             }
         }
     }
+    byte PaethPredictor(byte A, byte B, byte C)
+    {
+        byte P = A + B - C;
+        byte PA = Abs(P - A);
+        byte PB = Abs(P - B);
+        byte PC = Abs(P - C);
+        if (PA <= PB && PA <= PC) { return A; }
+        else if (PB <= PC) { return B; }
+        else { return C; }
+    }
+    void ReconstructImageData(Array<byte>& DecompressedStream, Array<byte>& UnfilteredStream)
+    {
+        size_t SamplesPerPixel = 0;
+        ColorFormat ColorType = GetColorType(Header.ColorType);
+        if (ColorType == ColorFormat::Grayscale) { SamplesPerPixel = 1; }
+        else if (ColorType == ColorFormat::Truecolor) { SamplesPerPixel = 3; }
+        else if (ColorType == ColorFormat::PaletteIndexed) { SamplesPerPixel = 1; }
+        else if (ColorType == ColorFormat::GrayscaleAlpha) { SamplesPerPixel = 2; }
+        else if (ColorType == ColorFormat::TruecolorAlpha) { SamplesPerPixel = 4; }
+        ASSERT(SamplesPerPixel > 0);
+        size_t BitsPerPixel = Header.BitDepth * SamplesPerPixel;
+        u32 Width = Header.Width;
+        ASSERT(Width * BitsPerPixel % 8 == 0); // TODO: Need to handle this case (bit padding)
+        size_t BytesPerScanline = 1 + (Width * BitsPerPixel / 8);
+        size_t BytesPerReconScanline = (Width * BitsPerPixel / 8);
+        int ActiveFilterMethod = 0;
+        for (size_t StreamIdx = 0; StreamIdx < DecompressedStream.Num; StreamIdx++)
+        {
+            bool bScanlineBegin = StreamIdx % BytesPerScanline == 0;
+            if (bScanlineBegin)
+            {
+                ActiveFilterMethod = DecompressedStream[StreamIdx];
+            }
+            else
+            {
+                byte X = DecompressedStream[StreamIdx];
+                if (ActiveFilterMethod != 0)
+                {
+                    // TODO(CKA): These calculations need to be adjusted for different bit depths / samples per pixel / etc
+                    bool bLeftColumnPx = (StreamIdx % BytesPerScanline) < (SamplesPerPixel + 1);
+                    bool bTopRowPx = (StreamIdx / BytesPerScanline) == 0;
+                    int CurrRow = (StreamIdx / BytesPerScanline);
+                    byte A = bLeftColumnPx ? 0 : UnfilteredStream[StreamIdx - SamplesPerPixel - CurrRow - 1];
+                    byte B = bTopRowPx ? 0 : UnfilteredStream[StreamIdx - BytesPerReconScanline - CurrRow - 1];
+                    byte C = (bLeftColumnPx || bTopRowPx) ? 0 : UnfilteredStream[StreamIdx - BytesPerReconScanline - SamplesPerPixel - CurrRow - 1];
+
+                    switch (ActiveFilterMethod)
+                    {
+                        case 1: { X = X + A; } break; // Sub
+                        case 2: { X = X + B; } break; // Up
+                        case 3: { X = X + ((A + B) / 2); } break; // Average
+                        case 4: { X = X + PaethPredictor(A, B, C); } break; // Paeth
+                        default: { ASSERT(false); } break;
+                    }
+                }
+                UnfilteredStream.Add(X);
+            }
+        }
+    }
     void ConstructImage(ImageT& OutImage)
     {
         ASSERT(bReadHeader && bReadEnd && ImageData.Num > 0);
@@ -335,92 +394,42 @@ struct PNGParseContext
 
         if (ImageData.Num > 0)
         {
-            PNGColorType ColorType = GetColorType(HeaderChunk.ColorType);
-            ASSERT(ColorType != PNGColorType::Invalid);
-            ASSERT(ColorType != PNGColorType::Truecolor || (HeaderChunk.BitDepth == 8 || HeaderChunk.BitDepth == 16));
-            ASSERT(ColorType != PNGColorType::PaletteIndexed || (HeaderChunk.BitDepth == 1 || HeaderChunk.BitDepth == 2 || HeaderChunk.BitDepth == 4 || HeaderChunk.BitDepth == 8));
-            ASSERT(!(ColorType == PNGColorType::GrayscaleAlpha || ColorType == PNGColorType::TruecolorAlpha) || (HeaderChunk.BitDepth == 8 || HeaderChunk.BitDepth == 16));
-            ASSERT(HeaderChunk.CompressionMethod == 0);
-            ASSERT(HeaderChunk.FilterMethod == 0);
-            ASSERT(HeaderChunk.InterlaceMethod == 0 || HeaderChunk.InterlaceMethod == 1);
-            bool bAdam7Interlace = HeaderChunk.InterlaceMethod == 1;
+            ColorFormat ColorType = GetColorType(Header.ColorType);
+            ASSERT(ColorType != ColorFormat::Invalid);
+            ASSERT(ColorType != ColorFormat::Truecolor || (Header.BitDepth == 8 || Header.BitDepth == 16));
+            ASSERT(ColorType != ColorFormat::PaletteIndexed || (Header.BitDepth == 1 || Header.BitDepth == 2 || Header.BitDepth == 4 || Header.BitDepth == 8));
+            ASSERT(!(ColorType == ColorFormat::GrayscaleAlpha || ColorType == ColorFormat::TruecolorAlpha) || (Header.BitDepth == 8 || Header.BitDepth == 16));
+            ASSERT(Header.CompressionMethod == 0);
+            ASSERT(Header.FilterMethod == 0);
+            ASSERT(Header.InterlaceMethod == 0 || Header.InterlaceMethod == 1);
+            bool bAdam7Interlace = Header.InterlaceMethod == 1;
             ASSERT(!bAdam7Interlace);
 
             Array<byte> DecompressedStream;
             DecompressImageData(ImageData, DecompressedStream);
             ASSERT(DecompressedStream.Num > 0);
 
-            OutImage.Width = HeaderChunk.Width;
-            OutImage.Height = HeaderChunk.Height;
+            OutImage.Width = Header.Width;
+            OutImage.Height = Header.Height;
             OutImage.PxCount = OutImage.Width * OutImage.Height;
             OutImage.PxBufferSize = sizeof(RGBA32) * OutImage.PxCount;
             OutImage.PxBuffer = new RGBA32[OutImage.PxCount];
 
             Array<byte> UnfilteredStream;
-            size_t SamplesPerPixel = 0;
-            if (ColorType == PNGColorType::Grayscale) { SamplesPerPixel = 1; }
-            else if (ColorType == PNGColorType::Truecolor) { SamplesPerPixel = 3; }
-            else if (ColorType == PNGColorType::PaletteIndexed) { SamplesPerPixel = 1; }
-            else if (ColorType == PNGColorType::GrayscaleAlpha) { SamplesPerPixel = 2; }
-            else if (ColorType == PNGColorType::TruecolorAlpha) { SamplesPerPixel = 4; }
-            size_t BitsPerPixel = HeaderChunk.BitDepth * SamplesPerPixel;
-            size_t BytesPerScanline = 1 + (OutImage.Width * BitsPerPixel / 8);
-            int ActiveFilterMethod = 0;
-            for (size_t StreamIdx = 0; StreamIdx < DecompressedStream.Num; StreamIdx++)
-            {
-                bool bScanlineBegin = StreamIdx % BytesPerScanline == 0;
-                if (bScanlineBegin)
-                {
-                    ActiveFilterMethod = DecompressedStream[StreamIdx];
-                }
-                else
-                {
-                    // TODO(CKA): These calculations are off
-                    //      They need to be adjusted for different bit depths / samples per pixel / etc
-                    bool bLeftColumnPx = StreamIdx % BytesPerScanline == 1;
-                    bool bTopRowPx = StreamIdx / BytesPerScanline == 0;
-                    byte X = DecompressedStream[StreamIdx];
-                    byte A = bLeftColumnPx ? 0 : DecompressedStream[StreamIdx - 1];
-                    byte B = bTopRowPx ? 0 : DecompressedStream[StreamIdx - BytesPerScanline];
-                    byte C = (bLeftColumnPx || bTopRowPx) ? 0 : DecompressedStream[StreamIdx - BytesPerScanline - 1];
+            ReconstructImageData(DecompressedStream, UnfilteredStream);
 
-                    switch (ActiveFilterMethod)
-                    {
-                        case 0: { } break; // None
-                        case 1: { X = X + A; } break; // Sub
-                        case 2: { X = X + B; } break; // Up
-                        case 3: { X = X + ((A + B) / 2); } break; // Average
-                        case 4: // Paeth
-                        {
-                            auto PaethPredictor = [](byte A, byte B, byte C) -> byte
-                            {
-                                byte P = A + B - C;
-                                byte PA = Abs(P - A);
-                                byte PB = Abs(P - B);
-                                byte PC = Abs(P - C);
-                                if (PA <= PB && PA <= PC) { return A; }
-                                else if (PB <= PC) { return B; }
-                                else { return C; }
-                            };
-                            X = X + PaethPredictor(A, B, C);
-                        } break;
-                        default: { ASSERT(false); } break;
-                    }
-                    UnfilteredStream.Add(X);
-                }
-            }
             ASSERT(UnfilteredStream.Num > 0);
             switch (ColorType)
             {
-                case PNGColorType::TruecolorAlpha:
+                case ColorFormat::TruecolorAlpha:
                 {
                     ASSERT(false);
                 } break;
-                case PNGColorType::GrayscaleAlpha:
+                case ColorFormat::GrayscaleAlpha:
                 {
                     ASSERT(false);
                 } break;
-                case PNGColorType::Truecolor:
+                case ColorFormat::Truecolor:
                 {
                     size_t StreamIdx = 0;
                     for (size_t PxIdx = 0; PxIdx < OutImage.PxCount; PxIdx++)
@@ -436,11 +445,11 @@ struct PNGParseContext
                     }
                     ASSERT(StreamIdx == UnfilteredStream.Num);
                 } break;
-                case PNGColorType::Grayscale:
+                case ColorFormat::Grayscale:
                 {
                     ASSERT(false);
                 } break;
-                case PNGColorType::PaletteIndexed:
+                case ColorFormat::PaletteIndexed:
                 {
                     ASSERT(false);
                 } break;
@@ -449,13 +458,15 @@ struct PNGParseContext
     }
     void CleanUp()
     {
-        if (PaletteChunk.Entries)
+        if (Palette.Entries)
         {
-            byte* PaletteChunkData = (byte*)PaletteChunk.Entries;
+            byte* PaletteChunkData = (byte*)Palette.Entries;
             delete[] PaletteChunkData;
         }
     }
 };
+
+}
 
 void LoadPNGFile(const char* Filename, ImageT& OutImage)
 {
@@ -463,7 +474,7 @@ void LoadPNGFile(const char* Filename, ImageT& OutImage)
     ASSERT(LoadedPNG.Contents && LoadedPNG.Size > 0);
     if (LoadedPNG.Contents && LoadedPNG.Size > 0)
     {
-        PNGParseContext Context{LoadedPNG, OutImage};
+        PNG::ParseContext Context{LoadedPNG, OutImage};
 
         delete[] LoadedPNG.Contents;
     }
